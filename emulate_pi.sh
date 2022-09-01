b81a8c5f0314817039b1b5818f3795818703c6df
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2022-09-02 02:12:36 +0300

    Removed defaults from qemu

--- emulate_pi.sh
+++ emulate_pi.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-qemu-system-aarch64 -kernel kernel.bin\
+QEMU=qemu-system-aarch64
+QEMU=/home/user-user/git/qemu/build/qemu-system-aarch64
+RUNCMD=$QEMU
+echo DEBUG_QEMU=$DEBUG_QEMU
+if [ -v DEBUG_QEMU ]; then
+	RUNCMD="gdb --args $QEMU"
+fi
+
+echo $RUNCMD
+$RUNCMD -kernel kernel.bin\
 	-machine raspi3b \
 	-nographic \
 	-serial stdio \
+	-nodefaults \
 	-s -S
+
