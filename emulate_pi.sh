1c69fb9dd906b22356e2feee6753be03dbb80646
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2024-03-14 22:32:10 +0300

    Scripts: exact qemu path is removed from scripts

--- emulate_pi.sh
+++ emulate_pi.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-QEMU=/home/user_user/git/qemu/build/qemu-system-aarch64
-# QEMU=qemu-system-aarch64
+QEMU=${QEMU_PATH:-qemu-system-aarch64}
 RUNCMD=$QEMU
-echo DEBUG_QEMU=$DEBUG_QEMU
 if [ -v DEBUG_QEMU ]; then
 	RUNCMD="gdb -silent -x debug_qemu.gdb --args $QEMU"
 fi
