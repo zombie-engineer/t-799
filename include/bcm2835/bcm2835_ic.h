c922e6aebc69a2a924feef4a8e193d1a6588e1ba
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2024-04-22 02:00:27 +0300

    bcm2835_ic: interrupt controller code cleaned up

--- include/bcm2835/bcm2835_ic.h
+++ include/bcm2835/bcm2835_ic.h
@@ -4,33 +4,36 @@
  * Information about some of the interrupt numbers is taken from
  * https://github.com/raspberrypi/linux/blob/rpi-3.12.y/arch/arm/mach-bcm2708/include/mach/platform.h
  * https://forums.raspberrypi.com/viewtopic.php?t=87106
+ * Also check this file in linux kernel
+ * Documentation/devicetree/bindings/interrupt-controller/brcm,bcm2835-armctrl-ic.txt
  */
 
 #define GPU_IRQ(__nr) __nr
 #define ARM_IRQ(__nr) (__nr + 64)
 #define IS_GPU_IRQ(__nr) (__nr < 64)
 
-#define BCM2835_IRQNR_SYSTIMER_0 GPU_IRQ(0)
-#define BCM2835_IRQNR_SYSTIMER_1 GPU_IRQ(1)
-#define BCM2835_IRQNR_SYSTIMER_2 GPU_IRQ(2)
-#define BCM2835_IRQNR_SYSTIMER_3 GPU_IRQ(3)
-#define BCM2835_IRQNR_USB        GPU_IRQ(9)
-#define BCM2835_IRQNR_DMA_0      GPU_IRQ(16)
-#define BCM2835_IRQNR_DMA_1      GPU_IRQ(17)
-#define BCM2835_IRQNR_DMA_2      GPU_IRQ(18)
-#define BCM2835_IRQNR_DMA_3      GPU_IRQ(19)
-#define BCM2835_IRQNR_DMA_4      GPU_IRQ(20)
-#define BCM2835_IRQNR_DMA_5      GPU_IRQ(21)
-#define BCM2835_IRQNR_DMA_6      GPU_IRQ(22)
-#define BCM2835_IRQNR_DMA_7      GPU_IRQ(23)
-#define BCM2835_IRQNR_DMA_8      GPU_IRQ(24)
-#define BCM2835_IRQNR_DMA_9      GPU_IRQ(25)
-#define BCM2835_IRQNR_DMA_10     GPU_IRQ(26)
-#define BCM2835_IRQNR_DMA_11     GPU_IRQ(27)
-#define BCM2835_IRQNR_DMA_12     GPU_IRQ(28)
-#define BCM2835_IRQNR_AUX        GPU_IRQ(29)
-#define BCM2835_IRQNR_UART0      GPU_IRQ(57)
-#define BCM2835_IRQNR_MAX        GPU_IRQ(63)
+#define BCM2835_IRQNR_SYSTIMER_0  GPU_IRQ(0)
+#define BCM2835_IRQNR_SYSTIMER_1  GPU_IRQ(1)
+#define BCM2835_IRQNR_SYSTIMER_2  GPU_IRQ(2)
+#define BCM2835_IRQNR_SYSTIMER_3  GPU_IRQ(3)
+#define BCM2835_IRQNR_USB         GPU_IRQ(9)
+#define BCM2835_IRQNR_DMA_0       GPU_IRQ(16)
+#define BCM2835_IRQNR_DMA_1       GPU_IRQ(17)
+#define BCM2835_IRQNR_DMA_2       GPU_IRQ(18)
+#define BCM2835_IRQNR_DMA_3       GPU_IRQ(19)
+#define BCM2835_IRQNR_DMA_4       GPU_IRQ(20)
+#define BCM2835_IRQNR_DMA_5       GPU_IRQ(21)
+#define BCM2835_IRQNR_DMA_6       GPU_IRQ(22)
+#define BCM2835_IRQNR_DMA_7       GPU_IRQ(23)
+#define BCM2835_IRQNR_DMA_8       GPU_IRQ(24)
+#define BCM2835_IRQNR_DMA_9       GPU_IRQ(25)
+#define BCM2835_IRQNR_DMA_10      GPU_IRQ(26)
+#define BCM2835_IRQNR_DMA_11      GPU_IRQ(27)
+#define BCM2835_IRQNR_DMA_12      GPU_IRQ(28)
+#define BCM2835_IRQNR_AUX         GPU_IRQ(29)
+#define BCM2835_IRQNR_UART0       GPU_IRQ(57)
+#define BCM2835_IRQNR_ARASAN_SDIO GPU_IRQ(62)
+#define BCM2835_IRQNR_MAX         GPU_IRQ(63)
 
 #define BCM2835_IRQNR_ARM_TIMER             ARM_IRQ(0)
 #define BCM2835_IRQNR_ARM_MAILBOX           ARM_IRQ(1)
