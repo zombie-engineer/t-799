0ee9abc8ed0266ab1aa1bd372a7b7fd3c1ac7ae7
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2024-05-19 22:47:40 +0300

    logging: printf is now protected by disabling interrupts
    This is enough to serialize printf usage across several tasks

--- include/printf.h
+++ include/printf.h
@@ -1,22 +1,31 @@
 #pragma once
 #include <uart_pl011.h>
 #include <stringlib.h>
+#include <cpu.h>
 
 extern char printfbuf[];
 extern size_t printfbuf_size;
 
 static inline const char * _printf(const char* fmt, ...)
 {
+  int irqflags;
+
   const char *res = fmt;
+
+  disable_irq_save_flags(irqflags);
   __builtin_va_list args;
   __builtin_va_start(args, fmt);
   vsnprintf(printfbuf, printfbuf_size, fmt, &args);
   uart_pl011_send(printfbuf, 0);
+  restore_irq_flags(irqflags);
   return res;
 }
 
 static inline void puts(const char *string)
 {
+  int irqflags;
+  disable_irq_save_flags(irqflags);
   uart_pl011_send(string, 0);
+  restore_irq_flags(irqflags);
 }
 #define printf _printf
